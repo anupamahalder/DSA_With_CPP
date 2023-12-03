19ceaf545f9083492be6e3b880a60034565764a2
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2023-12-03 16:55:45 +0530

    Create selection_sort.cpp

---
+++ selection_sort.cpp
@@ -0,0 +1,31 @@
+// ---------------Selection Sort--------- 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    // take array input 
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // sort array in ascending order using selection sort technique 
+    for(int i=0;i<n-1;i++){
+        // find the minimum element from the array 
+        int minIndex = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        // if minimum element is found then swap with the current element 
+        if(minIndex != i){
+            swap(arr[i], arr[minIndex]);
+        }
+    }
+    // print array after being sorted 
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
