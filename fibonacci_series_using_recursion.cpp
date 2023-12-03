ada6209fb19a573f20982529e96de66331d47930
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2023-12-03 16:57:10 +0530

    Print fibonacci series by getting each term from a recursive function

---
+++ fibonacci_series_using_recursion.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fibo(int n){
+    if(n<=1) return n;
+    return fibo(n-1)+fibo(n-2);
+}
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<fibo(i)<<" ";
+    }
+    return 0;
+}
