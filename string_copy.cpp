5ee9e484144c513e693c2a11b67c2749c03cc30f
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2023-12-07 18:16:13 +0530

    Copy one string to another string using '=' operator and index by index copying.

--- string_copy.cpp
+++ string_copy.cpp
@@ -10,10 +10,12 @@ int main(){
     for(int i=0;i<s1.size();i++){
         s2.push_back(s1[i]);
     }
+    // s2 is now contains the copy of s1 string
     cout<<s2<<endl;
     
     // copy using '=' (assignment operator) 
     s3 = s1;
+    // s3 is now contains the copy of s1 string
     cout<<s3<<endl;
     return 0;
 }
