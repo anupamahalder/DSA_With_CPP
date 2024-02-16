4e7f48941799a9938fb5f5284d8d32e07f40fba1
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-02-16 08:47:53 +0530

    Find total number of connected nodes to a node

--- Graph_Algorithms/Adjacency List Problems/Total_nodes_connected_to_a_node.cpp
+++ Graph_Algorithms/Adjacency List Problems/Total_nodes_connected_to_a_node.cpp
@@ -18,12 +18,52 @@ int main(){
     }
     int node;
     cin>>node;
-    int countNodes = 0;
-    // count the total nodes connected to a node 
-    for(int num: adj[node]){
-        countNodes++;
-    }
     // print the total number of nodes connected to a node 
-    cout<<"In node "<<node<<", total number of nodes: "<<countNodes<<endl;
+    cout<<"In node "<<node<<", total number of connected nodes: "<<adj[node].size()<<endl;
     return 0;
 }
+
+/*
+======================OUTPUT SECTION=============================
+Testcase 1:
+-------------
+INPUT: 
+      6 5
+      0 1
+      0 2
+      0 3
+      2 3
+      4 5
+      2
+OUTPUT:
+     In node 2, total number of connected nodes: 2
+----------------------------------------------
+Testcase 2:
+-------------
+INPUT: 
+     6 5
+     0 1
+     0 2
+     0 3
+     2 3
+     4 5
+     0
+OUTPUT:
+     In node 0, total number of connected nodes: 3
+-----------------------------------------------
+Testcase 3:
+------------
+INPUT:
+      7 7
+      0 1
+      1 2
+      2 3
+      1 3
+      4 0
+      0 5
+      5 6
+      1
+OUTPUT:
+     In node 1, total number of connected nodes: 3
+=================================================================
+*/
